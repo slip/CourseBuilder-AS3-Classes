5e4329d99db864f2a10f9559f7631f8d70171d3b
Ian Kennedy <exposure@gmail.com>
2012-08-14 19:39:12 -0400

    CourseUpdater.py prints .xsd files
    Signed-off-by: Ian Kennedy <exposure@gmail.com>

--- CourseUpdater.py
+++ CourseUpdater.py
@@ -1,12 +1,23 @@
-class CourseUpdater(object):
-	"""CourseUpdater will remove old lmsfiles and replace them with new ones"""
-	import os
-	def __init__(self, courseDirectory):
-		super(CourseUpdater, self).__init__()
-		self.courseDirectory = courseDirectory
-	
-	def removeOldFiles():
-		pass
+from os import listdir
+from fnmatch import fnmatch
 
-	def addNewFiles():
-		pass
+
+class CourseUpdater(object):
+    """CourseUpdater will remove old lmsfiles and replace them with new ones"""
+
+    def __init__(self, course_directory):
+        "initialize a new CourseUpdater instance"
+        self.course_directory = course_directory
+        self.course_files = listdir(self.course_directory)
+
+    def removeOldFiles(self):
+        pass
+
+    def addNewFiles(self):
+        pass
+
+cu = CourseUpdater("/Users/slip/Desktop/TST1001_NEW")
+print len(cu.course_files)
+for course_file in cu.course_files:
+    if fnmatch(course_file, '*.xsd'):
+        print course_file
