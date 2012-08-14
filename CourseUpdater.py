91044c15445a4eac49c3b11f95e95ed90da2b838
Ian Kennedy <exposure@gmail.com>
2012-08-14 16:41:45 -0400

    Add CourseUpdater.py
    Create a class to remove old lmsfiles and replace them with new, working ones.
    
    Signed-off-by: Ian Kennedy <exposure@gmail.com>

---
+++ CourseUpdater.py
@@ -0,0 +1,12 @@
+class CourseUpdater(object):
+	"""CourseUpdater will remove old lmsfiles and replace them with new ones"""
+	import os
+	def __init__(self, courseDirectory):
+		super(CourseUpdater, self).__init__()
+		self.courseDirectory = courseDirectory
+	
+	def removeOldFiles():
+		pass
+
+	def addNewFiles():
+		pass
