6c74a6a74d5af891a3574f0bce3fbd4f57e005b4
Ian Kennedy <exposure@gmail.com>
2012-08-27 10:31:57 -0400

    CourseUpdater removes files.
    Signed-off-by: Ian Kennedy <exposure@gmail.com>

--- CourseUpdater.py
+++ CourseUpdater.py
@@ -1,5 +1,7 @@
-from os import listdir
-from subprocess import call
+#!/usr/bin/python
+
+import sys
+import os
 from xml.dom.minidom import parseString
 
 
@@ -12,37 +14,36 @@ class CourseUpdater(object):
 
     def __init__(self, course_directory):
         "initialize a new CourseUpdater instance"
-        self.course_directory = course_directory
-        self.course_files = listdir(self.course_directory)
+        self.course_directory = sys.argv[1]
+        self.course_files = os.listdir(self.course_directory)
 
-    def getCourseTitle(self):
+    def get_course_title(self):
         "traverse imsmanifest.xml and extract the course title"
-        manifestLocation = "%s/imsmanifest.xml" % self.course_directory
+        manifestLocation = os.path.join(self.course_directory, "imsmanifest.xml")
         manifestFile = open(manifestLocation, 'r')
         manifestData = manifestFile.read()
         manifestFile.close()
         manifestDom = parseString(manifestData)
         self.course_title = manifestDom.getElementsByTagName('title')[0].firstChild.data
-        print self.course_title
-        return self.course_title
 
-    def removeOldFiles(self):
+    def remove_old_files(self):
         "deletes legacy lms files to be replaced"
         for keeper in self.files_to_keep:
             print "removing %s from %s" % (keeper, self.course_files)
             self.course_files.remove(keeper)
+        for file in self.course_files:
+            file = os.path.join(self.course_directory, file)
+            print "deleting %s from %s" % (file, self.course_files)
+            os.remove(file)
 
-    def addNewFiles(self):
+    def add_new_files(self):
         "replaces deleted files with new working files"
 
-cu = CourseUpdater("/Users/slip/Desktop/GAMETEST")
-print "course_directory: %s \ncourse_title = %s" % (cu.course_directory, cu.getCourseTitle())
-call("imagesnap")
-# print cu.course_directory
-# print "here are all of the files in the directory:"
-# print cu.course_files
-# print "removing files_to_keep"
-# cu.removeOldFiles()
-# print "here are all of the remaining files"
-# for remainingFile in cu.course_files:
-#     print remainingFile
+
+def main():
+    cu = CourseUpdater("/Users/slip/Desktop/GAMETEST_copy")
+    print "course_directory: %s \ncourse_title = %s" % (cu.course_directory, cu.get_course_title())
+    cu.remove_old_files()
+
+if __name__ == '__main__':
+    main()
